3b66c5630aa51d1137c0f0e9a0f57d93f19c9deb
Emma Follender <efollender@gmail.com>
2015-04-20 08:55:44 -0400

    Ch-ch-ch-changesss.

--- app/components/home/Chart.js
+++ app/components/home/Chart.js
@@ -1,26 +1,31 @@
 import {React} from 'reapp-kit';
 const AreaChart = require('react-d3/areachart').AreaChart;
 
-let dateObj = new Date();
-
 export default class Chart extends React.Component {
   constructor(props){
     super(props);
   }
   render(){
+    let areaData = [{name:'lows', values: []}, {name:'highs', values: []}];
     let maxData = this.props.data.map((day,i)=>{
-      
-      return {x: i ,y: day.temp.max};
+      areaData[0].values.push([i+1, day.temp.max]);
     });
     let minData = this.props.data.map((day,i)=>{
-      let dateObj = Date.prototype.getDay(parseInt(i));
-      return {x: i ,y: day.temp.min};
+      areaData[1].values.push([i+1, day.temp.min]);
     });
-    let areaData = [{name:'lows', values: minData}, {name:'highs', values: maxData}];
     console.log(areaData);
     return (
       <AreaChart
         data={areaData}
+        yAccessor={d=>{
+          return d[1];
+        }}
+        xAccessor={d=>{
+          return d[0];
+        }}
+        xAxisTickCount={7}
+        width={400}
+        height={300}
       />
     );
   }
